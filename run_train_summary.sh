46947a4bd538a81108d126eb8527f67760c2f81e
hi-i-m-gtooth <noface21105@gmail.com>
2022-11-30 21:38:08 +0800

    prevent OOM

--- run_train_summary.sh
+++ run_train_summary.sh
@@ -3,8 +3,8 @@ accelerate launch summary.py \
     --text_column "maintext" \
     --summary_column "title" \
     --source_prefix "summarize: " \
-    --per_device_train_batch_size 6 \
-    --per_device_eval_batch_size 6 \
+    --per_device_train_batch_size 2 \
+    --per_device_eval_batch_size 2 \
     --learning_rate 5e-5 \
     --num_train_epochs 50 \
     --max_length 512 \
